26b563d84ac7fba82b5bbaecc8785cb8f12fe644
jamacio <jamacio.rocha@justdigital.com.br>
2021-08-28 12:44:25 -0300

    add project

---
+++ packages/venia-ui/lib/components/SearchBar/__tests__/suggestedCategories.spec.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { createTestInstance } from '@magento/peregrine';
+import SuggestedCategories from '../suggestedCategories';
+
+jest.mock('react-router-dom', () => ({
+    Link: jest.fn(() => null),
+    useLocation: jest.fn()
+}));
+
+useLocation.mockReturnValue(globalThis.location);
+
+const categories = [
+    { label: 'A', value_string: 'a' },
+    { label: 'B', value_string: 'b' },
+    { label: 'C', value_string: 'c' },
+    { label: 'D', value_string: 'd' },
+    { label: 'E', value_string: 'e' }
+];
+
+test('renders correctly', () => {
+    const instance = createTestInstance(
+        <SuggestedCategories categories={categories} value="foo" />
+    );
+
+    expect(instance.toJSON()).toMatchSnapshot();
+});
+
+test('renders a max of 4 categories by default', () => {
+    const { root } = createTestInstance(
+        <SuggestedCategories categories={categories} value="foo" />
+    );
+
+    expect(root.findAllByType(Link)).toHaveLength(4);
+});
+
+test('allows the render limit to be configured', () => {
+    const { root } = createTestInstance(
+        <SuggestedCategories categories={categories} limit={2} value="foo" />
+    );
+
+    expect(root.findAllByType(Link)).toHaveLength(2);
+});
---
+++ venia-integration-tests/src/fixtures/index.js
@@ -0,0 +1,9 @@
+export * as accountAccess from './accountAccess';
+export * as categoryPage from './categoryPage';
+export * as cartPage from './cartPage';
+export * as googleMapApi from './googleMapApi';
+export * as homePage from './homePage';
+export * as myAccountMenu from './myAccountMenu';
+export * as productPage from './productPage';
+export * as wishlist from './wishlist';
+export * as graphqlMockedCalls from './graphqlMockedCalls';
